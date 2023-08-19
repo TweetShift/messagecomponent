aeb2c328919987f5fed2219330caab777aa1044a
rjt <rjt.rockx@gmail.com>
2023-08-19 19:57:04 +0530

    fix @import locations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,10 @@
 <script lang="ts">
+	import Message from '$lib/components/message';
+	import Messages from '$lib/components/messages';
 </script>
 
 <h1>hello world</h1>
+
+<Messages lightTheme>
+	<Message>Hello world!</Message>
+</Messages>
